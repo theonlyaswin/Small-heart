e383ae228eab82d9669de5ca884bda7a964967d8
aswinofficial <achu2003aswin@gmail.com>
2022-01-19 23:14:03 +0530

    minior changes🍟

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/home")
+def home():
+    return render_template('index.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
